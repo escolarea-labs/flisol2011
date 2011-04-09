0c2269d48dde90a7fcc387d30b04946b53de9eb5
Fernando Irías Escher <fernando.irias@escolarea.com>
2011-04-08 18:41:38 -0600

    Template base para el proyecto

--- urls.py
+++ urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.defaults import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = patterns('',
     # Examples:
@@ -15,3 +16,6 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
+
+
+urlpatterns += staticfiles_urlpatterns()
