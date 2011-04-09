bf492d16e47c6143d2ded55ad50e2f2b2c234bdb
Fernando Irías Escher <fernando.irias@escolarea.com>
2011-04-09 02:50:53 -0600

    Correcciones finales y separado de archivos estáticos

--- settings.py
+++ settings.py
@@ -78,6 +78,8 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
     os.path.join(ROOT_PATH, 'static'),
+    os.path.join(ROOT_PATH, 'lista','static'),
+
 )
 
 # List of finder classes that know how to find static files in
